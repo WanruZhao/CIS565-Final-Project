5b46c040b6e8070f5c6ee1e869b2b183d3845a1f
yuruwang <yuruwang@seas.upenn.edu>
2018-11-17 18:51:45 -0500

    setup simple scene

--- src/rendering/gl/passes/GBufferPass.ts
+++ src/rendering/gl/passes/GBufferPass.ts
@@ -14,26 +14,7 @@ class GBufferPass extends ShaderProgram {
 		this.use();
 	}
 
-  	drawElements(camera: Camera, drawables: Array<Drawable>, tex: Texture) {
-	    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-		gl.enable(gl.DEPTH_TEST);
-	
-		let model = mat4.create();
-		let viewProj = mat4.create();
-		let view = camera.viewMatrix;
-		let proj = camera.projectionMatrix;
-		let color = vec4.fromValues(0.5, 0.5, 0.5, 1);
-	
-		mat4.identity(model);
-		mat4.multiply(viewProj, camera.projectionMatrix, camera.viewMatrix);
-		this.setModelMatrix(model);
-		this.setViewProjMatrix(viewProj);
-		this.setGeometryColor(color);
-		this.setViewMatrix(view);
-		this.setProjMatrix(proj);
-		this.bindTexToUnit("tex_Color", tex, 0);
-		
-	
+  	drawElements(camera: Camera, drawables: Array<Drawable>) {	
 		for (let drawable of drawables) {
 			super.draw(drawable);
 		}
